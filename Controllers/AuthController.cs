dba466b4b23878a7c1bc06569db607751dfac5d7
m4ujo <mauriciocast11@outlook.com>
2025-01-30 18:49:09 -0500

    fear: add User and AuthController class

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using learning_jwt_auth_dotnet.Entities;
+using learning_jwt_auth_dotnet.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learning_jwt_auth_dotnet.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    public static User user = new();
+
+    [HttpPost("register")]
+    public ActionResult<User> Register(UserDto request)
+    {
+      var hashedPassword = new PasswordHasher<User>()
+        .HashPassword(user, request.Password);
+
+      user.Username = request.Username;
+      user.PasswordHash = hashedPassword;
+
+      return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public ActionResult<string> Login(UserDto request)
+    {
+      if (user.Username != request.Username)
+        return BadRequest("User not found");
+
+      if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+        return BadRequest("Wrong password");
+
+      string token = "Success";
+
+      return Ok(token);
+    }
+  }
+}
